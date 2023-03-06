0d1e45021fbc8a672ab2e3d18e178fb2799eaff2
Callme7liwa <ayoub.seddiki@usmba.ac.ma>
2023-03-06 19:42:16 +0100

    some old input validation (login ac + signup ac ) + welcome avtivity after login + add new baby activity

---
+++ app/src/main/java/ensias/myteam/babytakingcare/AddBabyActivity.java
@@ -0,0 +1,14 @@
+package ensias.myteam.babytakingcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBabyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_baby);
+    }
+}
